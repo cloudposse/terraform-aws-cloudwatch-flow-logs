be703e7d1e7281a6056ddb89cacca62facfaacde
aknysh <andriy.knysh@gmail.com>
2020-12-07 17:39:36 -0500

    Updates

--- variables.tf
+++ variables.tf
@@ -1,36 +1,3 @@
-variable "name" {
-  default     = ""
-  description = "Name  (e.g. `bastion` or `db`)"
-}
-
-variable "namespace" {
-  description = "Namespace (e.g. `cp` or `cloudposse`)"
-  type        = "string"
-}
-
-variable "stage" {
-  description = "Stage (e.g. `prod`, `dev`, `staging`)"
-  type        = "string"
-}
-
-variable "delimiter" {
-  type        = "string"
-  default     = "-"
-  description = "Delimiter to be used between `name`, `namespace`, `stage`, etc."
-}
-
-variable "attributes" {
-  type        = "list"
-  default     = []
-  description = "Additional attributes (e.g. `policy` or `role`)"
-}
-
-variable "tags" {
-  type        = "map"
-  default     = {}
-  description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)"
-}
-
 variable "region" {
   description = "AWS region"
   default     = ""
@@ -94,9 +61,4 @@ variable "filter_pattern" {
 variable "kms_key_id" {
   description = "ID of KMS key"
   default     = ""
-}
-
-variable "enabled" {
-  default     = "true"
-  description = "Set to false to prevent the module from creating anything"
 }
