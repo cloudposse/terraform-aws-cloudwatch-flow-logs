0883fe1619bbca54f0d93bb47b6d95452b6e1863
Alex Smith <38281006+asmithwp@users.noreply.github.com>
2021-04-01 17:29:35 +0100

    Revert version
    It conflicts with a module

--- versions.tf
+++ versions.tf
@@ -3,8 +3,7 @@ terraform {
   required_version = ">= 0.12.0, < 0.14.0"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 2.70.0"
+      source = "hashicorp/aws"
     }
   }
 }
