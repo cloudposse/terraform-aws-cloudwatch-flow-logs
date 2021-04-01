ad3730d65d9c1029a7e7dbada9124c98171f80ea
Alex Smith <38281006+asmithwp@users.noreply.github.com>
2021-04-01 17:18:14 +0100

    Pin provider version to v2

--- versions.tf
+++ versions.tf
@@ -3,7 +3,8 @@ terraform {
   required_version = ">= 0.12.0, < 0.14.0"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 2.70.0"
     }
   }
 }
